0a77cd6b9d99f819edf3c0fe8c8567e69068f30a
orihikarna <yonemoto.akihiro@gmail.com>
2020-05-20 12:24:41 +0900

    generic tapping

--- keymap.c
+++ keymap.c
@@ -14,6 +14,8 @@ enum custom_keycodes {
     ADJUST,
 };
 
+#define R_T_ENT 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
@@ -26,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | Shift |   Z   |   X   |   C   |   V   |   B   |       |                    |       |   N   |   M   |   ,   |   .   |   /   |   \   |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |       |Raz/Ent|Sft/Spc|       |       |       |       |       |
+   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |Raz/Ent|Sft/Spc|       |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -34,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,                      KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_MINS, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,                      KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,                      KC_NO,   KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      KC_NO, RAISE, KC_RSFT,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE, KC_RSFT, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 
   /* Lower
@@ -47,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | Shift |       |       |       |  Win  |PrntScr|       |                    |       |   ^   |   ¥   |   ,   |   .   |   /   |   ]   |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |       |Raz/Ent|Sft/Spc|       |       |       |       |       |
+   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |Raz/Ent|Sft/Spc|       |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_LOWER] = LAYOUT(
@@ -55,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, KC_NO,                      KC_NO,   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_AT,   S(JP_MINS), \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,                      KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_LBRC, \
     KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_LGUI, KC_PSCR, KC_NO,                      KC_NO,   JP_CIRC, JP_YEN,  JP_COMM, JP_DOT,  JP_SLSH, JP_RBRC, \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      KC_NO, RAISE, KC_RSFT,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE, KC_RSFT, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 
   /* Raise
@@ -68,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | Shift |   F1  |   F2  |   F3  |   F4  |   F5  |       |                    |       |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |       |Raz/Ent|Sft/Spc|       |       |       |       |       |
+   * |       |       |       |       |       |  Alt  |  Ctrl |Low/Del|    |Raz/Ent|Sft/Spc|       |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_RAISE] = LAYOUT(
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, KC_NO,                      KC_NO,   KC_NO,   KC_HOME, KC_PGUP, KC_PGDN, KC_END,  S(JP_MINS), \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,                      KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F12,  \
     KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,                      KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      KC_NO, RAISE, KC_RSFT,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE, KC_RSFT, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 
   /* Adjust
@@ -111,15 +113,20 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 typedef struct {
+    uint16_t key1;
+    uint8_t key2;
     bool pressed;
     uint16_t time;
 } mytap_t;
 
-static mytap_t tap_lower = { false, 0 };
-static mytap_t tap_raise = { false, 0 };
-static mytap_t tap_rsft = { false, 0 };
+static mytap_t taps[] = {
+    { LOWER,   KC_DEL, false, 0 },
+    { RAISE,   KC_ENT, false, 0 },
+    { KC_RSFT, KC_SPC, false, 0 },
+};
+#define TAP_COUNT (sizeof( taps) / sizeof( taps[0] ))
 
-bool process_record_user_default( uint16_t keycode, keyrecord_t* record ) {
+static bool process_record_layer( uint16_t keycode, keyrecord_t* record ) {
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
@@ -132,17 +139,9 @@ bool process_record_user_default( uint16_t keycode, keyrecord_t* record ) {
             if (record->event.pressed) {
                 layer_on(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
-                tap_lower.pressed = true;
-                tap_lower.time = record->event.time;
             } else {
                 layer_off(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
-                if (tap_lower.pressed) {
-                    tap_lower.pressed = false;
-                    if (TIMER_DIFF_16( record->event.time, tap_lower.time ) < TAPPING_TERM) {
-                        tap_code( KC_DEL );
-                    }
-                }
             }
             return false;
             break;
@@ -150,17 +149,9 @@ bool process_record_user_default( uint16_t keycode, keyrecord_t* record ) {
             if (record->event.pressed) {
                 layer_on(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
-                tap_raise.pressed = true;
-                tap_raise.time = record->event.time;
             } else {
                 layer_off(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
-                if (tap_raise.pressed) {
-                    tap_raise.pressed = false;
-                    if (TIMER_DIFF_16( record->event.time, tap_raise.time ) < TAPPING_TERM) {
-                        tap_code( KC_ENT );
-                    }
-                }
             }
             return false;
             break;
@@ -172,13 +163,13 @@ bool process_record_user_default( uint16_t keycode, keyrecord_t* record ) {
             }
             return false;
             break;
-        default:
-            if (record->event.pressed) {
-                tap_lower.pressed = false;
-                tap_raise.pressed = false;
-            }
-            break;
     }
+    return true;
+}
+
+bool _process_record_user( uint16_t keycode, keyrecord_t* record ) {
+    if (process_record_layer( keycode, record ) == false) return false;
+    if (process_record_emacs( keycode, record ) == false) return false;
     return true;
 }
 
@@ -186,28 +177,26 @@ static keyrecord_t pressed = {{{0,0},true,0}, {0,0,0,0,0}};
 static keyrecord_t depressed = {{{0,0},false,0}, {0,0,0,0,0}};
 
 bool process_record_user( uint16_t keycode, keyrecord_t* record ) {
-    if (process_record_user_default( keycode, record ) == false) return false;
-    switch (keycode) {
-        case KC_RSFT:
+    for (uint8_t n = 0; n < TAP_COUNT; ++n) {
+        mytap_t* tap = &taps[n];
+        if (tap->key1 == keycode) {
             if (record->event.pressed) {
-                tap_rsft.pressed = true;
-                tap_rsft.time = record->event.time;
-            } else if (tap_rsft.pressed) {
-                tap_rsft.pressed = false;
-                if (TIMER_DIFF_16( record->event.time, tap_rsft.time ) < TAPPING_TERM) {
-                    if (process_record_emacs( keycode, record )) unregister_code( keycode );
-                    if (process_record_emacs( KC_SPC, &pressed )) register_code( KC_SPC );
-                    if (process_record_emacs( KC_SPC, &depressed )) unregister_code( KC_SPC );
+                tap->pressed = true;
+                tap->time = record->event.time;
+            } else if (tap->pressed) {
+                tap->pressed = false;
+                if (true) {// TIMER_DIFF_16( record->event.time, tap->time ) < 1000) {
+                    if (_process_record_user( keycode,   record )) unregister_code( keycode );
+                    if (_process_record_user( tap->key2, &pressed )) register_code( tap->key2 );
+                    if (_process_record_user( tap->key2, &depressed )) unregister_code( tap->key2 );
                     return false;
                 }
             }
-            break;
-        default:
+        } else {
             if (record->event.pressed) {
-                tap_rsft.pressed = false;
+                tap->pressed = false;
             }
-            break;
+        }
     }
-    if (process_record_emacs( keycode, record ) == false) return false;
-    return true;
+    return _process_record_user( keycode, record );
 }
