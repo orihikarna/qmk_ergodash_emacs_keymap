f662de3df99d445a9cc53764a35d6cc7c643331a
orihikarna <yonemoto.akihiro@gmail.com>
2020-05-26 13:51:41 +0900

    swapped Sfift and Space

--- keymap.c
+++ keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | Shift |   Z   |   X   |   C   |   V   |   B   |       |                    |       |   N   |   M   |   ,   |   .   |  / >  |  \ _  |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |  ^ ~  |       |       |       |       |Alt/Del|  Ctrl |       |    |       |Sft/Ent|Raz/Spc|       |       |       |       |  ¥ |  |
+   * |  ^ ~  |       |       |       |       |Alt/Del|  Ctrl |       |    |       |Raz/Spc|Sft/Ent|       |       |       |       |  ¥ |  |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,                      KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_MINS, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_LBRC,                    JP_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,                      KC_NO,   KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
-    JP_CIRC, KC_NO,   KC_NO,   KC_NO,            AT_DEL,  KC_LCTL, KC_NO,    KC_NO,   ST_ENT,  RT_SPC,           KC_NO,   KC_NO,   KC_NO,   JP_YEN   \
+    JP_CIRC, KC_NO,   KC_NO,   KC_NO,            AT_DEL,  KC_LCTL, KC_NO,    KC_NO,   RT_SPC,  ST_ENT,           KC_NO,   KC_NO,   KC_NO,   JP_YEN   \
   ),
 
   /* Raise
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | Shift |       | RGB ON| BL ON |  Win  |PrntScr|       |                    |       |       |  Left |   Up  |  Down | Right |   \   |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |  ^ ~  |       |       |       |       |Alt/Del|  Ctrl |       |    |       |Sft/Ent|Raz/Spc|       |       |       |       |  ¥ |  |
+   * |  ^ ~  |       |       |       |       |Alt/Del|  Ctrl |       |    |       |Raz/Spc|Sft/Ent|       |       |       |       |  ¥ |  |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_RAISE] = LAYOUT(
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, KC_NO,                      KC_NO,   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_AT,   KC_F11,  \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    JP_LBRC,                    JP_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,  \
     KC_LSFT, KC_NO,   RGB_TOG, BL_TOGG, KC_LGUI, KC_PSCR, KC_NO,                      KC_NO,   KC_NO,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, JP_BSLS, \
-    JP_CIRC, KC_NO,   KC_NO,   KC_NO,            AT_DEL,  KC_LCTL, KC_NO,    KC_NO,   ST_ENT,  RT_SPC,           KC_NO,   KC_NO,   KC_NO,   JP_YEN   \
+    JP_CIRC, KC_NO,   KC_NO,   KC_NO,            AT_DEL,  KC_LCTL, KC_NO,    KC_NO,   RT_SPC,  ST_ENT,           KC_NO,   KC_NO,   KC_NO,   JP_YEN   \
   ),
 
   /* Adjust
