da395e2fcc13cd8bfbbba6807a74c75fb480a3b5
orihikarna <yonemoto.akihiro@gmail.com>
2020-06-15 17:21:53 +0900

    removed pinky bottom

--- keymap.c
+++ keymap.c
@@ -25,19 +25,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * |  ESC  |   Q   |   W   |   E   |   R   |   T   |       |                    |       |   Y   |   U   |   I   |   O   |   P   |  - =  |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * |  Tab  |   A   |   S   |   D   |   F   |   G   |   [   |                    |   ]   |   H   |   J   |   K   |   L   |  ; +  |  : *  |
+   * | Shift |   A   |   S   |   D   |   F   |   G   |  ^ ~  |                    |  ¥ |  |   H   |   J   |   K   |   L   |  ; +  |  : *  |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * | Shift |   Z   |   X   |   C   |   V   |   B   |       |                    |       |   N   |   M   |   ,   |   .   |  / >  |  \ _  |
+   * |  Tab  |   Z   |   X   |   C   |   V   |   B   |       |                    |       |   N   |   M   |   ,   |   .   |  / >  |  \ _  |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |  ^ ~  |       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |  ¥ |  |
+   * |       |       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,                      KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,   \
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,                      KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_MINS, \
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_LBRC,                    JP_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,                      KC_NO,   KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
-    JP_CIRC, KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,    RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   JP_YEN   \
+    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_CIRC,                    JP_YEN,  KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
+    KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,                      KC_NO,   KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,    RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 
   /* Raise
@@ -46,19 +46,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * |PrntScr|   !   |   "   |   #   |   $   |   %   |       |                    |       |   &   |   '   |   (   |   )   |   @   |   =   |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * |  Win  |   1   |   2   |   3   |   4   |   5   |  Del  |                    | Enter |   6   |   7   |   8   |   9   |   0   |   *   |
+   * |  Win  |   1   |   2   |   3   |   4   |   5   |  Del  |                    | Enter |   6   |   7   |   8   |   9   |   0   |   [   |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * |  Caps |  Home | PageUp| PageDn|  End  |  F11  |       |                    |       |  F12  |  Left |   Up  |  Down | Right |   _   |
+   * |  Caps |  Home | PageUp| PageDn|  End  |  F11  |       |                    |       |  F12  |  Left |   Up  |  Down | Right |   ]   |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * | Insert|       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |   |   |
+   * |       |       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_RAISE] = LAYOUT(
     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,                      KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   \
     KC_PSCR, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, KC_NO,                      KC_NO,   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_AT,   S(JP_MINS), \
-    KC_LGUI, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,                     KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    S(JP_COLN), \
-    KC_CAPS, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_F11,  KC_NO,                      KC_NO,   KC_F12,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, S(JP_BSLS), \
-    KC_INS,  KC_NO, KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   S(JP_YEN)   \
+    KC_LGUI, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,                     KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_LBRC, \
+    KC_CAPS, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_F11,  KC_NO,                      KC_NO,   KC_F12,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, JP_RBRC, \
+    KC_NO,   KC_NO, KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 
   /* Lower
@@ -67,19 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
    * | RGB ON|   !   |   "   |   #   |   $   |   %   |       |                    |       |   &   |   '   |   (   |   )   |   @   |   =   |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * |  MODE |   1   |   2   |   3   |   4   |   5   |   BS  |                    | Enter |   6   |   7   |   8   |   9   |   0   |   *   |
+   * |  MODE |   1   |   2   |   3   |   4   |   5   |   BS  |                    | Enter |   6   |   7   |   8   |   9   |   0   |   [   |
    * |-------+-------+-------+-------+-------+-------+-------+                    +-------+-------+-------+-------+-------+-------+-------|
-   * | BL ON |  Home | PageUp| PageDn|  End  |  F11  |       |                    |       |  F12  |  Left |   Up  |  Down | Right |   _   |
+   * | BL ON |  Home | PageUp| PageDn|  End  |  F11  |       |                    |       |  F12  |  Left |   Up  |  Down | Right |   ]   |
    * |-------+-------+-------+-------+-------+-------+-------+-------+    +-------+------ +-------+-------+-------+-------+-------+-------|
-   * |  BRTG |       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |   |   |
+   * |       |       |       |       |       |  Alt  |  Ctrl | Lower |    | Raise | Shift | Space |       |       |       |       |       |
    * ,-------------------------------+       +-----------------------+    +-----------------------+       +-------------------------------.
    */
   [_LOWER] = LAYOUT(
     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,                      KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   \
     RGB_TOG, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, KC_NO,                      KC_NO,   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_AT,   S(JP_MINS), \
-    RGB_MOD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,                    KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    S(JP_COLN), \
-    BL_TOGG, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_F11,  KC_NO,                      KC_NO,   KC_F12,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, S(JP_BSLS), \
-    BL_BRTG, KC_NO, KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   S(JP_YEN)   \
+    RGB_MOD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,                    KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_LBRC, \
+    BL_TOGG, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_F11,  KC_NO,                      KC_NO,   KC_F12,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, JP_RBRC, \
+    KC_NO,   KC_NO, KC_NO,   KC_NO,            KC_LALT, KC_LCTL, LOWER,      RAISE,   KC_RSFT, KC_SPC,           KC_NO,   KC_NO,   KC_NO,   KC_NO    \
   ),
 };
 
